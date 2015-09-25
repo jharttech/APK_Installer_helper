9d2000960e1cdbb34229d33db13e05b4bf015aaa
Jason Hartgraves <jharttech@hotmail.com>
2015-09-25 14:59:26 -0500

    Initial Commit of mg_apk_script_generic file.

---
+++ mg_apk_script_generic.sh
@@ -0,0 +1,43 @@
+#! /bin/bash
+
+##############################################################################
+# Set some global variables
+_APK_SERVER="FIXME"
+
+##############################################################################
+# Changing Directory for ease of paths
+cd ~/
+
+# Installing ADB on linux machine for use in script
+sudo apt-get install android-tools-adb:i386 -y
+
+# Start the adb server
+sudo adb start-server
+
+##############################################################################
+		wget "$_APK_SERVER"/apk/Socrative_20Student_4.3.0_apk-dl.com.apk
+                wget "$_APK_SERVER"/apk/MobiLock_20Kiosk_20Lockdown_2.1.1_apk-dl.com.apk
+ 		wget "$_APK_SERVER"/apk/WiFi Manager_3.5.4.2_apk-dl.com.apk
+		wget "$_APK_SERVER"/apk/20150925_tabletbg-mgbg.png
+                
+# Script for downloading designated apk files and images for installation on 
+# multiple devices to be deployed
+for device in `adb devices | grep -v "List" | sed -e "s/\t.*$//"`;
+do
+		
+	echo "Installing app to $device";
+		adb -s $device install ~/Socrative_20Student_4.3.0_apk-dl.com.apk;
+    
+		adb -s $device install ~/MobiLock_20Kiosk_20Lockdown_2.1.1_apk-dl.com.apk;
+     
+	echo "pushing background PNG to $device's Gallery";
+		adb -s $device push ~/20150925_tabletbg-mgbg.png /mnt/sdcard/Download/;
+     
+done
+
+##############################################################################
+# Removing temporary files from machine to prepare for next device.
+
+	echo "Removing temporary files from machine"
+		rm -r ~/*.apk
+		rm -r ~/20150925_tabletbg-mgbg.png
